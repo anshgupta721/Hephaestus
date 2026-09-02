9e7c42d9e931b5f507954702057fb4338dcf438f
Ansh Gupta <anshgupta721@gmail.com>
2026-09-02 12:54:57 -0500

    added many crates within cargo and also worked on the simulation stepping crate. The simulation stepper now has a config for defining a LTVSystem and all essential configurations for the simulation itself

--- crates/simulation/src/stepper.rs
+++ crates/simulation/src/stepper.rs
@@ -1,13 +1,36 @@
+use dynamics::models::state_space_model::StateSpace;
+use onboard_software::control_stack::ControlStack;
+use onboard_software::config::SensorVector;
+use crate::config::{Plant, ControlVector, StateVector, NX, NU, NY};
 
 
-
-
-pub fn sim_step(dt: float){ // need to add initial conditions, 
-
-    let plant_state_n_minus_1 = Dynamics.sense(dt);
-    let plant_state_n = Dynamics.step(dt, plant_state_n_minus_1);
-
-    let onboard_state_n_minus_1 = Onboard_Software.sense();
-    let onboard_state_n = Onboard_Software.estimate();
-    let onboard_controls_n = Onboard_Software.control();
+pub fn sim_dynamics(t: f64, plant: &StateSpace<Plant, NX, NU, NY>, u_prev: ControlVector, x_prev: StateVector, dx_prev: StateVector) -> StateVector{ // need to add initial conditions, 
+    // replace with rk4 eventually
+    plant.derivative(t, x_prev, u_prev, dx_prev)
 }
+
+pub fn sensor_dynamics(x: StateVector) -> SensorVector{
+    SensorVector::zeros()
+}
+
+pub fn sim_stepper(x_0: StateVector, u_0: ControlVector, t: f64, t_span: [f64; 2], dt: f64, mut gnc: ControlStack){
+
+    let plant = StateSpace::new(Plant);
+    let mut t = t_span[0];
+    let mut x = x_0;
+    let sensor_0 = sensor_dynamics(x);
+    gnc.estimator.initialize(sensor_0);
+    while t < t_span[1]{
+        // initial state and initial sensor data should be passed in
+        
+
+        let sensor_n = sensor_dynamics(x);
+        let onboard_state_n = gnc.estimator.estimate(sensor_n);// sensor_state_minus_1
+        let onboard_controls_n = gnc.controller.control(); //onboard_state_n
+
+
+        t += dt;
+    }
+
+}
+
