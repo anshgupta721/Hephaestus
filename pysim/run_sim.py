fe94b99ccb50c4a8a2cac4436e907ea07a33e178
Ansh Gupta <anshgupta721@gmail.com>
2026-09-03 17:09:56 -0500

    pylint ignored the rust crate

--- pysim/run_sim.py
+++ pysim/run_sim.py
@@ -1,7 +1,7 @@
 """Run the simulation entry point."""
 import numpy as np
 import matplotlib.pyplot as plt
-from pyfrontend import pysim_runner
+from pyfrontend import pysim_runner  # pylint: disable=no-name-in-module
 
 if __name__ == "__main__":
     x_0 = np.zeros(3, dtype=np.float64)
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     t, states, _ = pysim_runner(x_0, u_0, (0.0, 10.0), 0.01)
     plt.plot(t, states[:, 0])
     plt.show()
+
     
