71bc7001a9c3b882c0b6e7f39505770ef41961c0
Ansh Gupta <anshgupta721@gmail.com>
2026-09-03 17:02:07 -0500

    pylint hcecks

--- pysim/run_sim.py
+++ pysim/run_sim.py
@@ -1,6 +1,8 @@
+"""Run the simulation entry point."""
 import numpy as np
 import matplotlib.pyplot as plt
 from pyfrontend import pysim_runner
 
 if __name__ == "__main__":
     pysim_runner()
+    
